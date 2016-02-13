0c5505ea5cc3c31b46e75d8ceba140393998fc31
Lonny Strunk <lstrunk2@alaska.edu>
2016-02-12 16:37:17 -0900

    Deleted all of the unnecessary files. Added all of the category pictures. Added a UINavigationController with toolbar and created the main screen with a UICollectionView.

---
+++ iOS/TimeOfNeed/TimeOfNeed/CategoryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryCollectionViewCell.swift
+//  TimeOfNeed
+//
+//  Created by Lonny Strunk on 2/8/16.
+//  Copyright © 2016 TON. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+}
